3b0897df4f5f13f2af00cee1f679bb50c20182a1
Tomas <tomas.gonzalez@prosegur.com>
2021-03-04 20:34:24 -0300

    active style for links

--- web/components/header.js
+++ web/components/header.js
@@ -1,8 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
 import { AiOutlineFacebook, AiOutlineInstagram } from "react-icons/ai";
 
+const activeClassStyle = "text-white bg-red-500";
+
 export default function Header() {
+  const router = useRouter();
+  // Functions returns true for active page
+  const isActive = useCallback(
+    (url) => {
+      return router.pathname.indexOf(url) > -1;
+    },
+    [router.pathname],
+  )
   return (
     <header className="flex flex-col w-full mx-auto bg-gray-100 border-b-2 border-red-400 shadow-2xl">
       <div className="w-full py-2 text-white bg-red-700 shadow-xl">
@@ -32,22 +44,34 @@ export default function Header() {
       </div>
       <nav className="flex items-center justify-center bg-gray-100 border-t border-gray-500">
         <Link href="/">
-          <a>Inicio</a>
+          <a>
+            Inicio
+          </a>
+        </Link>
+        <Link href="/servicios">
+          <a className={(isActive("/servicios") && activeClassStyle) || ''}>
+            Servicios
+          </a>
         </Link>
         <Link href="/noticias">
-          <a>Servicios</a>
-        </Link>
-        <Link href="/noticias">
-          <a>Noticias</a>
+          <a className={(isActive("/noticias") && activeClassStyle) || ''}>
+            Noticias
+          </a>
         </Link>
         <Link href="/cuerpo-activo">
-          <a className="text-white bg-red-500">Cuerpo activo</a>
+          <a className={(isActive("/cuerpo-activo") && activeClassStyle) || ''}>
+            Cuerpo activo
+          </a>
         </Link>
         <Link href="/comision-directiva">
-          <a>Comisión directiva</a>
+          <a className={(isActive("/comision-directiva") && activeClassStyle) || ''}>
+            Comisión directiva
+          </a>
         </Link>
         <Link href="/contacto">
-          <a>Contacto</a>
+          <a className={(isActive("/contacto") && activeClassStyle) || ''}>
+            Contacto
+          </a>
         </Link>
       </nav>
     </header>
