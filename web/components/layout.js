a0eb83a73351efff704b1fe6db382966b90c9e34
Tomas <tomasg88@gmail.com>
2021-03-21 21:20:46 -0300

    #19 rrss data from _app to header

--- web/components/layout.js
+++ web/components/layout.js
@@ -3,12 +3,12 @@ import Header from "../components/header"
 import Meta from "../components/meta"
 import Offcanvas from "../components/offcanvas"
 import SimpleReactLightbox from "simple-react-lightbox"
-export default function Layout({ children }) {
+export default function Layout({ rrss, children }) {
   return (
     <>
       <Meta />
       <Offcanvas />
-      <Header />
+      <Header rrss={rrss} />
       <div className="min-h-screen bg-red-500">
         <SimpleReactLightbox>
           <main>{children}</main>
