8706b2c43cff5618918712841c99927a103ab88c
Tomas <tomasg88@gmail.com>
2021-03-26 19:01:54 -0300

    use article content only to parse body

--- web/components/articleContent.js
+++ web/components/articleContent.js
@@ -8,19 +8,22 @@ export default function ArticleContent({ title, mainImage, dateString, body }) {
   const date = parseISO(dateString)
   return (
     <>
-      <div className="relative flex flex-col-reverse items-center justify-center w-full overflow-hidden md:text-center md:text-white md:bg-gray-800 " style={{minHeight: "70vh"}}>
-        <div className="relative z-10 flex flex-col items-center justify-center w-full max-w-3xl px-3 font-sans text-6xl font-light">
-          <div className="w-full pt-3 mb-6 text-lg">
-            <time className="uppercase" dateTime={dateString}>
-              {format(date, 'LLLL	d, yyyy', { locale: es })}
-            </time>
+      {
+        title && mainImage && dateString &&
+          <div className="relative flex flex-col-reverse items-center justify-center w-full overflow-hidden md:text-center md:text-white md:bg-gray-800 " style={{minHeight: "70vh"}}>
+            <div className="relative z-10 flex flex-col items-center justify-center w-full max-w-3xl px-3 font-sans text-6xl font-light">
+              <div className="w-full pt-3 mb-6 text-lg">
+                <time className="uppercase" dateTime={dateString}>
+                  {format(date, 'LLLL	d, yyyy', { locale: es })}
+                </time>
+              </div>
+              <h1 className="pb-3 mb-3">{title}</h1>
+            </div>
+            <div className="inset-0 z-0 cursor-pointer md:absolute md:opacity-40">
+              <CoverImage title={title} image={mainImage} />
+            </div>
           </div>
-          <h1 className="pb-3 mb-3">{title}</h1>
-        </div>
-        <div className="inset-0 z-0 cursor-pointer md:absolute md:opacity-40">
-          <CoverImage title={title} image={mainImage} />
-        </div>
-      </div>
+      }
       <div className="max-w-3xl px-3 mx-auto mt-6 prose prose-lg md:prose-xl">
         <article className={styles.article}>
           <BlockContent
