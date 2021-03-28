00e22ad34f9222a75166429bcdcfa89631e6f966
Tomas <tomasg88@gmail.com>
2021-03-28 20:45:06 -0300

    comision y cuerpo content styles

--- web/pages/cuerpo-activo.js
+++ web/pages/cuerpo-activo.js
@@ -10,7 +10,7 @@ import { BG_CONSTANTS } from "../utils/constants"
 export default function CuerpoActivo({ list }) {
   return (
     <Layout>
-      <div className="min-h-screen bg-white">
+      <div className="min-h-screen bg-gray-100">
         <div className="relative py-64 overflow-hidden text-center text-white bg-gray-800 ">
           <h1 className="relative z-10 mb-6 font-sans text-2xl uppercase font-bolder">
             Cuerpo Activo
@@ -26,9 +26,14 @@ export default function CuerpoActivo({ list }) {
           </AnchorLink>
           <BackgroundImage image={BG_CONSTANTS.team} />
         </div>
-        <div id="integrantes" className="bg-white">
-          <h2 className="max-w-2xl px-5 pt-12 mx-auto text-6xl font-light text-left text-gray-400">Integrantes</h2>
-          <div className="grid max-w-2xl grid-cols-3 p-2 mx-auto mt-6 bg-white ">
+
+        <div id="integrantes" className="max-w-6xl p-12 mx-auto mt-6 ">
+          <div className="flex items-center justify-between max-w-6xl pt-12 pb-6 mx-auto font-sans border-b-2 border-yellow-400 md:flex-row">
+            <h3 className="text-5xl font-light text-center text-gray-900 md:text-left">
+              Integrantes
+            </h3>
+          </div>
+          <div className="grid max-w-6xl gap-3 px-2 py-6 pb-24 mx-auto mt-6 md:grid-cols-3 sm:grid-cols-2">
             {list &&
               list.map((n) => (
                 <Hero key={n._id} name={n.title} description={n.rank} image={n.image} />
