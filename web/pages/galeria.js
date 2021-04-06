24f3550ca31618f342a03372243478adbf99a1e7
Tomas <tomasg88@gmail.com>
2021-04-05 21:14:50 -0300

    new AlbumCover component

--- web/pages/galeria.js
+++ web/pages/galeria.js
@@ -1,31 +1,11 @@
 import React, { useState } from "react"
 import BackgroundImage from "../components/backgroundImage"
 import Layout from "../components/layout"
-import { urlForImage } from "../lib/sanity"
 import { getClient } from "../lib/sanity.server"
 import { albumsQuery } from "../lib/queries"
 import Gallery from "../components/gallery"
 import { BG_CONSTANTS } from "../utils/constants"
-
-function AlbumCover({ album, selectAlbum }) {
-  const [showDetails, setShowDetails] = useState(false)
-  return (
-    <div className="relative flex items-start justify-start mb-3 overflow-hidden border border-gray-300 rounded-sm shadow ">
-      <img
-        onMouseEnter={() => setShowDetails(true)}
-        onMouseLeave={() => setShowDetails(false)}
-        
-        className="object-cover w-48 h-48 cursor-pointer opacity-90"
-        src={urlForImage(album.cover).url()}
-      />
-      <div className="relative z-10 pt-3 pl-3 text-gray-900">
-        <div className="text-xl">{album.title}</div>
-        <div className="text-base">{album.description}</div>
-        <div onClick={() => selectAlbum(album.imageList)} className="mt-3 font-bold text-red-600 uppercase duration-150 cursor-pointer hover:opacity-80">abrir galeria</div>
-      </div>
-    </div>
-  )
-}
+import AlbumCover from "../components/albumCover"
 
 export default function Galeria({ albums }) {
   const [selectedAlbum, setSelectedAlbum] = useState([])
@@ -40,7 +20,13 @@ export default function Galeria({ albums }) {
         </div>
         <div className="flex flex-col max-w-2xl mx-auto mt-2 bg-white">
           {albums?.map((a) => (
-            <AlbumCover key={a._id} album={a} selectAlbum={setSelectedAlbum} />
+            <AlbumCover 
+              key={a._id}
+              title={a.title}
+              cover={a.cover} 
+              album={a.imageList} 
+              selectAlbum={setSelectedAlbum} 
+            />
           ))}
         </div>
         {selectedAlbum.length > 0 && (
