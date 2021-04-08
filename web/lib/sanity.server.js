31032d68d94516800d373c25218454c395be3d55
Tomas <tomasg88@gmail.com>
2021-04-08 17:26:54 -0300

    remove unused method

--- web/lib/sanity.server.js
+++ web/lib/sanity.server.js
@@ -15,18 +15,3 @@ export const previewClient = createClient({
 })
 
 export const getClient = (preview) => (preview ? previewClient : sanityClient)
-
-export function overlayDrafts(docs) {
-  const documents = docs || []
-  const overlayed = documents.reduce((map, doc) => {
-    if (!doc._id) {
-      throw new Error('Ensure that `_id` is included in query projection')
-    }
-
-    const isDraft = doc._id.startsWith('drafts.')
-    const id = isDraft ? doc._id.slice(7) : doc._id
-    return isDraft || !map.has(id) ? map.set(id, doc) : map
-  }, new Map())
-
-  return Array.from(overlayed.values())
-}
