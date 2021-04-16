2f672d3b5baebfd77fc434218bcd3ea8a3b7fb7b
Tomas <tomasg88@gmail.com>
2021-04-16 15:21:26 -0300

    horizontal card

--- web/components/horizontalCard.js
+++ web/components/horizontalCard.js
@@ -13,11 +13,11 @@ const getMonth = (date) => format(parseISO(date), 'LLL', { locale: es })
 // title, mainImage, excerpt, slug
 export default function HorizontalCard({ title, mainImage, excerpt, slug, publishedAt }) {
   return (
-    <div className="relative flex h-28 overflow-hidden duration-500 transform translate-y-0 bg-white rounded-md shadow-xl border-b-4 border-red-600 mb-4">
-      <div className="min-w-16 w-1/3 h-full">
+    <div className="relative flex flex-col w-full mb-4 duration-500 transform translate-y-0 bg-white border-b-4 border-red-600 rounded-md shadow-xl sm:flex-row">
+      <div className="flex-none sm:w-64 rounded-tl-md">
         {mainImage ? (
           <Link href={`/noticias/${slug}`}>
-            <a aria-label={title} className="transition-all duration-500 hover:opacity-80">
+            <a aria-label={title} className="w-64 h-64 transition-all duration-500 hover:opacity-80 rounded-tl-md">
               <CoverImage title={title} image={mainImage} />
             </a>
           </Link>
@@ -25,12 +25,12 @@ export default function HorizontalCard({ title, mainImage, excerpt, slug, publis
           <></>
         )}
       </div>
-      <div className="xs:px-2 px-6 pt-6 h-full">
-        <h3 className="font-sans text-lg font-bold md:text-xl xs:truncate">{title}</h3>
-        <p className="xs:hidden my-2 text-xs line-clamp-4 truncate">{excerpt}</p>
+      <div className="px-6 pt-6 xs:px-2">
+        <h3 className="font-sans text-lg font-bold md:text-3xl">{title}</h3>
+        <p className="my-2 text-lg line-clamp-2 xs:hidden">{excerpt}</p>
       </div>
-      <div className="flex absolute bottom-0 left-0 bg-red-600 text-white items-center p-2 py-1 rounded-tr-md">
-        <p className="text-xl mr-2"> 
+      <div className="absolute top-0 left-0 flex items-center p-2 py-1 text-white bg-red-600 rounded-br-md rounded-tl-md">
+        <p className="mr-2 text-xl"> 
           {getDate(publishedAt)} 
         </p>
         <p className="text-sm"> 
