98dce485141bcec500b42c0278b7bc94d3ed1116
santuan <smrcouto88@gmail.com>
2021-05-09 17:14:44 -0300

    fix de tarjetas news academia y equipamiento

--- web/components/albumCover.js
+++ web/components/albumCover.js
@@ -4,7 +4,7 @@ import { MdPhotoCamera } from "react-icons/md";
 
 export default function AlbumCover({ cover, title, description, album, selectAlbum }) {
   return (
-    <div className="relative h-64 my-3 duration-700 transform translate-y-1 rounded shadow-md from-gray-800 via-gray-800 to-gray-900 bg-gradient-to-t hover:bg-opacity-90 hover:translate-y-0 hover:shadow-xl">
+    <div className="relative my-3 duration-700 transform translate-y-1 rounded shadow-md h-96 from-gray-800 via-gray-800 to-gray-900 bg-gradient-to-t hover:bg-opacity-90 hover:translate-y-0 hover:shadow-xl">
       <div className="absolute inset-0 overflow-hidden rounded-md">
         <img
           onClick={() => selectAlbum(album)}
@@ -14,7 +14,7 @@ export default function AlbumCover({ cover, title, description, album, selectAlb
       </div>
       <div onClick={() => selectAlbum(album)} className="relative z-10 flex flex-col items-center justify-end w-full h-full text-left text-gray-100 cursor-pointer ">
         <div className="w-full px-4 mb-2 text-2xl font-bold">{title}</div>
-        <div className="w-full px-4 text-lg">{description}</div>
+        <div className="w-full px-4 text-lg line-clamp-3">{description}</div>
         <div className="flex items-center justify-start w-full p-2 mx-3 mt-2 font-bold uppercase duration-700 bg-red-600 shadow-2xl bg-opacity-20 hover:translate-x-2 hover:bg-red-500">
           <MdPhotoCamera className="w-6 h-6 m-2"/>
           <span className="ml-1">Abrir galería</span>
