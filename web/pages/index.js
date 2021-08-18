e7506c2be845608a6f9b3d5b83182151f5f0a117
Tomás <tomasg88@gmail.com>
2021-08-18 16:11:39 -0300

    prettier and 2 prettified pages

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,52 +1,52 @@
-import Head from "next/head"
-import styles from "../styles/Home.module.css"
-import Layout from "../components/layout"
-import HomeCarousel from "../components/Heros/HeroHomeCarousel"
-import Card from "../components/Cards/cardNews"
-import Link from "next/link"
-import { getClient } from "../lib/sanity.server"
-import { indexQuery } from "../lib/queries"
-import Fade from "react-reveal/Fade"
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Layout from '../components/layout';
+import HomeCarousel from '../components/Heros/HeroHomeCarousel';
+import Card from '../components/Cards/cardNews';
+import Link from 'next/link';
+import { getClient } from '../lib/sanity.server';
+import { indexQuery } from '../lib/queries';
+import Fade from 'react-reveal/Fade';
 
 export default function Home({ recentNews }) {
-  return (
-    <Layout>
-      <div className={styles.container}>
-        <main className="w-full ">
-          <HomeCarousel arrows={false} />
-          <div className="bg-gray-100">
-            <div className="flex flex-col items-center justify-center max-w-6xl pt-24 pb-6 mx-auto font-sans border-b-2 border-yellow-400 md:flex-row">
-              <Fade cascade>
-                <h2 className="text-5xl font-light text-center text-gray-900 ">
-                  Últimas noticias
-                </h2>
-              </Fade>
+    return (
+        <Layout>
+            <div className={styles.container}>
+                <main className="w-full ">
+                    <HomeCarousel arrows={false} />
+                    <div className="bg-gray-100">
+                        <div className="flex flex-col items-center justify-center max-w-6xl pt-24 pb-6 mx-auto font-sans border-b-2 border-yellow-400 md:flex-row">
+                            <Fade cascade>
+                                <h2 className="text-5xl font-light text-center text-gray-900 ">
+                                    Últimas noticias
+                                </h2>
+                            </Fade>
+                        </div>
+                        <div className="grid max-w-6xl gap-3 p-8 pb-12 mx-auto mt-6 lg:grid-cols-3 sm:grid-cols-2">
+                            {recentNews &&
+                                recentNews.map((n) => (
+                                    <Card {...n} key={n._id} />
+                                ))}
+                        </div>
+                        <div className="flex flex-col items-center justify-between max-w-6xl pb-24 mx-auto font-sans">
+                            <Link href="/noticias">
+                                <a className="relative z-10 px-8 py-3 mt-3 text-2xl font-bold text-red-600 bg-gray-100 border-b-2 border-red-600 hover:bg-red-600 hover:text-white ">
+                                    Ver todas las Noticias
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                </main>
             </div>
-              <div className="grid max-w-6xl gap-3 p-8 pb-12 mx-auto mt-6 lg:grid-cols-3 sm:grid-cols-2">
-                {recentNews &&
-                  recentNews.map((n) => (
-                    <Card {...n} key={n._id} />
-                  ))}
-              </div>
-            <div className="flex flex-col items-center justify-between max-w-6xl pb-24 mx-auto font-sans">
-              <Link href="/noticias">
-                <a className="relative z-10 px-8 py-3 mt-3 text-2xl font-bold text-red-600 bg-gray-100 border-b-2 border-red-600 hover:bg-red-600 hover:text-white ">
-                  Ver todas las Noticias
-                </a>
-              </Link>
-            </div>
-          </div>
-        </main>
-      </div>
-    </Layout>
-  )
+        </Layout>
+    );
 }
 
 export async function getStaticProps(ctx) {
-  const { recentNews } = await getClient(false).fetch(indexQuery)
-  return {
-    props: {
-      recentNews
-    },
-  }
+    const { recentNews } = await getClient(false).fetch(indexQuery);
+    return {
+        props: {
+            recentNews,
+        },
+    };
 }
