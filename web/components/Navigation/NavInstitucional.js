104baeff21df6ccf1b60677aa5ca27fb610808ab
Tomás <tomasg88@gmail.com>
2021-08-18 18:59:21 -0300

    Revert "remove unused components"
    This reverts commit f029c626c8ebbeb216c4a1ce4e904af34f6b2715.

---
+++ web/components/Navigation/NavInstitucional.js
@@ -0,0 +1,12 @@
+import HeaderLink from '../Navigation/HeaderLink';
+
+export default function HeroInstitucional() {
+    return (
+        <>
+            <HeaderLink url="/comision-directiva" title="Comisión Directiva" />
+            <HeaderLink url="/cuerpo-activo" title="Cuerpo Activo" />
+            <HeaderLink url="/equipamiento" title="Equipamiento" />
+            <HeaderLink url="/institucional" title="Historia" />
+        </>
+    );
+}
