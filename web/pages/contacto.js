e61014d9e208df7ebc5e9224b9a07eb1a40c626b
Tomás <tomasg88@gmail.com>
2021-10-12 17:01:19 -0300

    extract component to separate file

--- web/pages/contacto.js
+++ web/pages/contacto.js
@@ -1,27 +1,13 @@
-import styles from 'styles/Home.module.css';
-import Layout from 'components/Layout/Layout';
-import BackgroundImage from 'components/BackgroundImage/BackgroundImage';
-import { getClient } from 'lib/sanity.server';
-import { contactDataQuery } from 'lib/queries';
-import { Context } from 'components/context';
-import { useContext } from 'react';
-import RrssIcon from 'components/RrssIcon/RrssIcon';
-import { BG_CONSTANTS, MAPS_URL } from 'utils/constants';
-
-const ContactItem = ({ title, value }) => (
-    <div className="px-12 border-t border-red-400">
-        <dl>
-            <div className="px-4 py-5 text-xl text-center md:text-left sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                <dt className="mt-1 text-base font-medium text-gray-500 uppercase">
-                    {title}
-                </dt>
-                <dd className="mt-1 text-gray-900 select-all sm:mt-0 sm:col-span-2">
-                    {value}
-                </dd>
-            </div>
-        </dl>
-    </div>
-);
+import styles from "styles/Home.module.css";
+import Layout from "components/Layout/Layout";
+import BackgroundImage from "components/BackgroundImage/BackgroundImage";
+import { getClient } from "lib/sanity.server";
+import { contactDataQuery } from "lib/queries";
+import { Context } from "components/context";
+import { useContext } from "react";
+import RrssIcon from "components/RrssIcon/RrssIcon";
+import ContactItem from "components/ContactItem/ContactItem";
+import { BG_CONSTANTS, MAPS_URL } from "utils/constants";
 
 export default function Contact({ phones, mails }) {
     const [rrss] = useContext(Context);
@@ -31,7 +17,7 @@ export default function Contact({ phones, mails }) {
                 <div className="relative flex items-start justify-center w-full pb-32 overflow-hidden text-left text-gray-800 bg-gray-900 md:pt-12 md:items-start ">
                     <div className="relative z-50 w-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-2xl md:mt-12">
                         <h1 className="relative z-10 flex flex-col items-center justify-between px-10 pt-12 pb-4 font-sans text-6xl font-light text-gray-700 bg-gray-100 pattern-vertical-lines-x ">
-                            Contacto{' '}
+                            Contacto{" "}
                             <small className="mt-3 text-lg font-bold opacity-80">
                                 24hs / 365 días
                             </small>
@@ -60,9 +46,9 @@ export default function Contact({ phones, mails }) {
                                             Redes sociales
                                         </dt>
                                         <dd className="flex items-center justify-center mt-1 text-gray-900 md:justify-start sm:mt-0">
-                                            {rrss.map((rs) => (
+                                            {rrss?.map((rs) => (
                                                 <RrssIcon
-                                                    className={'mr-6 text-3xl'}
+                                                    className={"mr-6 text-3xl"}
                                                     key={rs._id}
                                                     rrss={rs.rrss}
                                                     url={rs.rrssUrl}
@@ -80,7 +66,7 @@ export default function Contact({ phones, mails }) {
                             loading="lazy"
                         ></iframe>
                     </div>
-                    <BackgroundImage image={BG_CONSTANTS.doors} opacity={20} />
+                    <BackgroundImage image={BG_CONSTANTS.doors} />
                 </div>
             </div>
         </Layout>
