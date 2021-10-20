ff1a38fdae6107856e8774a57cb8da862696d32f
Tomás <tomasg88@gmail.com>
2021-10-20 16:17:35 -0300

    Button unit test

--- web/__tests__/Button.test.js
+++ web/__tests__/Button.test.js
@@ -2,28 +2,33 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Button from "../components/Button/Button";
 
-describe("Button", () => {
+describe("<Button />", () => {
     const testProps = {
         text: "Prueba",
         href: "/noticias",
     };
-    it("renders a button", () => {
+
+    test("renders a button", () => {
         const cmp = render(<Button {...testProps} />);
         expect(cmp.container).toHaveTextContent(testProps.text);
     });
 
-    it("changes style on hover", () => {
-        const cmp = render(<Button {...testProps} />);
-
-        const button = cmp.getByText(testProps.text);
-        expect(button).toBeInTheDocument();
-        expect(button).toHaveAttribute("target", "_self");
-    });
-
-    it("redirects to google", () => {
+    test("redirects to google", () => {
         const cmp = render(<Button {...testProps} />);
 
         const button = cmp.getByText(testProps.text);
         expect(button).toHaveAttribute("href", testProps.href);
+        expect(button).toHaveAttribute("target", "_self");
+    });
+
+    test("throws error due to missing prop-types", () => {
+        let cmp;
+        try {
+            cmp = render(<Button text={testProps.text} />);
+        } catch (error) {
+            console.log("ERROR: ", error);
+        }
+
+        expect(cmp).toBeUndefined();
     });
 });
