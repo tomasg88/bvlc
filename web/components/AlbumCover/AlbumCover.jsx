a3fea962fddbdf7bb9161ed5eac250adc1f19dee
Tomás <tomasg88@gmail.com>
2021-10-27 19:14:16 -0300

    use next image

--- web/components/AlbumCover/AlbumCover.jsx
+++ web/components/AlbumCover/AlbumCover.jsx
@@ -1,31 +1,40 @@
 import React from 'react';
-import { urlForImage } from 'lib/sanity';
+import Image from 'next/image';
 import { MdPhotoCamera } from 'react-icons/md';
 import styles from './AlbumCover.module.scss';
 import PropTypes from 'prop-types';
 import { sanityImagePropType } from 'utils/sanityPropType';
+import { sanityConfig } from 'lib/config';
+import { useNextSanityImage } from 'next-sanity-image';
 
-const AlbumCover = ({ cover, title, description, album, selectAlbum }) => (
-    <div className={styles.root}>
-        <div className={styles.imageContainer}>
-            <img
+const AlbumCover = ({ cover, title, description, album, selectAlbum }) => {
+    const { src, loader } = useNextSanityImage(sanityConfig, cover);
+
+    return (
+        <div className={styles.root}>
+            <div className={styles.imageContainer}>
+                <Image
+                    loader={loader}
+                    src={src}
+                    layout="fill"
+                    objectFit="cover"
+                    className={styles.img}
+                />
+            </div>
+            <div
                 onClick={() => selectAlbum(album)}
-                src={urlForImage(cover).url()}
-            />
-        </div>
-        <div
-            onClick={() => selectAlbum(album)}
-            className={styles.infoContainer}
-        >
-            <div className={styles.title}>{title}</div>
-            <div className={styles.description}>{description}</div>
-            <div className={styles.ctaButton}>
-                <MdPhotoCamera className={styles.icon} />
-                <span className={styles.buttonText}>Abrir galería</span>
+                className={styles.infoContainer}
+            >
+                <div className={styles.title}>{title}</div>
+                <div className={styles.description}>{description}</div>
+                <div className={styles.ctaButton}>
+                    <MdPhotoCamera className={styles.icon} />
+                    <span className={styles.buttonText}>Abrir galería</span>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 AlbumCover.propTypes = {
     title: PropTypes.string.isRequired,
