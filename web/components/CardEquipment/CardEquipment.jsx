ff4a626b4d3f14526b4bc0c5208e7b264352aebe
Tomás <tomasg88@gmail.com>
2021-10-27 19:49:29 -0300

    use next image cmp

--- web/components/CardEquipment/CardEquipment.jsx
+++ web/components/CardEquipment/CardEquipment.jsx
@@ -1,35 +1,44 @@
 import React from 'react';
-import { urlForImage } from 'lib/sanity';
+import Image from 'next/image';
 import { MdPhotoCamera } from 'react-icons/md';
 import BlockContent from '@sanity/block-content-to-react';
 import styles from './CardEquipment.module.scss';
 import PropTypes from 'prop-types';
+import { useNextSanityImage } from 'next-sanity-image';
+import { sanityConfig } from 'lib/config';
 
-const CardEquipment = ({ cover, title, body, onClick }) => (
-    <div className={styles.card}>
-        <div className={styles.image}>
-            <img
-                className={styles.img}
-                onClick={onClick}
-                src={urlForImage(cover).url()}
-            />
-        </div>
-        <div className={styles.content}>
-            <div className={styles.title}>{title}</div>
-            <BlockContent
-                blocks={body}
-                className={styles.excerpt}
-                projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
-                imageOptions={{ w: 1200, fit: 'fill' }}
-                dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
-            />
-            <div onClick={onClick} className={styles.action}>
-                <MdPhotoCamera className="w-6 h-6 " />
-                <span className="ml-3">Ver galería</span>
+const CardEquipment = ({ cover, title, body, onClick }) => {
+    const { src, loader } = useNextSanityImage(sanityConfig, cover);
+    return (
+        <div className={styles.card}>
+            <div className={styles.image}>
+                <Image
+                    loader={loader}
+                    src={src}
+                    layout="fill"
+                    objectFit="cover"
+                    className={styles.img}
+                    onClick={onClick}
+                    alt={title}
+                />
+            </div>
+            <div className={styles.content}>
+                <div className={styles.title}>{title}</div>
+                <BlockContent
+                    blocks={body}
+                    className={styles.excerpt}
+                    projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+                    imageOptions={{ w: 1200, fit: 'fill' }}
+                    dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+                />
+                <div onClick={onClick} className={styles.action}>
+                    <MdPhotoCamera className="w-6 h-6 " />
+                    <span className="ml-3">Ver galería</span>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 CardEquipment.propTypes = {
     title: PropTypes.string.isRequired,
