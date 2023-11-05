302d345d1508abf36617328415ed7ab12fa2ab4f
Emanuel Garcia <wemagw@gmail.com>
2023-11-05 15:33:28 -0300

    sanity studio V3 upgrade

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -1,15 +1,4 @@
-//
-// Para activar los cambios y subirlos al CMS de Prod
-//
-// sanity graphql deploy
-// sanity deploy
-//
-
-// First, we must import the schema creator
-import createSchema from "part:@sanity/base/schema-creator";
-// Then import schema types from any plugins that might expose them
-import schemaTypes from "all:part:@sanity/base/schema-type";
-
+// schemas/schema.js
 // We import object and document schemas
 import blockContent from "./blockContent";
 import category from "./category";
@@ -25,27 +14,17 @@ import youtube from "./youtube";
 import specialty from "./specialty";
 
 // Then we give our schema to the builder and provide the result to Sanity
-export default createSchema({
-  // We name our schema
-  name: "default",
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
-    general,
-    post,
-    page,
-    author,
-    leadership,
-    activeForce,
-    category,
-    equipment,
-    specialty,
-    album,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
-    youtube,
-  ]),
-});
+export default [
+  general,
+  post,
+  page,
+  author,
+  leadership,
+  activeForce,
+  category,
+  equipment,
+  specialty,
+  album,
+  blockContent,
+  youtube,
+];
