e0152fd5c8b6be84493a391ad6896d14e11e115b
Tomás González <tomasg88@gmail.com>
2023-11-20 17:51:14 -0300

    move test with their components

--- components/Button/Button.test.js
+++ components/Button/Button.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Button from '../components/Button/Button';
+import Button from './Button';
 
 describe('<Button />', () => {
   const testProps = {
