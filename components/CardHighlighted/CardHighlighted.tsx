d4f9c94b9571667427d4d763d43e9f6db85c4e01
Tomás González <tomasg88@gmail.com>
2023-12-03 15:58:13 -0300

    replace useWindowSizes by react-use

--- components/CardHighlighted/CardHighlighted.tsx
+++ components/CardHighlighted/CardHighlighted.tsx
@@ -3,19 +3,18 @@ import React, { FC } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useNextSanityImage } from 'next-sanity-image';
+import { useWindowSize } from 'react-use';
+import Fade from 'react-reveal/Fade';
+
 import { sanityConfig } from 'lib/config';
 import styles from './CardHighlighted.module.scss';
-import Fade from 'react-reveal/Fade';
-import useWindowSize from 'hooks/useWindowSizes';
 import { News } from 'types/News';
 
 const CardHighlighted: FC<News> = ({ title, slug, mainImage }): JSX.Element => {
-  const { width, isMobile } = useWindowSize();
-  const imageHeight = isMobile || width <= 500 ? 1000 : 300;
-
-  const imgProps = useNextSanityImage(sanityConfig, mainImage, {
-    imageBuilder: () => urlForImage(mainImage).size(900, imageHeight),
-  });
+  const { width } = useWindowSize();
+  const imageHeight = width <= 500 ? 1000 : 300;
+  console.log({ width, imageHeight });
+  const imgProps = useNextSanityImage(sanityConfig, mainImage);
 
   return (
     <Fade>
