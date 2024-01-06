6b3f3bd0a2789c3ad0de4c6178c9c41b3c5f9200
Tomás González <tomasg88@gmail.com>
2024-01-06 19:53:22 -0300

    Feat/final linting (#134)
    * disable rule
    
    * address lint msgs

--- studio/scripts/migration.js
+++ studio/scripts/migration.js
@@ -1,14 +1,14 @@
-import sanityClient from "part:@sanity/base/client";
+import sanityClient from 'part:@sanity/base/client';
 
 const client = sanityClient.withConfig({ apiVersion: 1 });
 
-const OLD_TYPE = "equipment"; // review the _type we want to change
-const NEW_TYPE = "specialty"; // target _type
+const OLD_TYPE = 'equipment'; // review the _type we want to change
+const NEW_TYPE = 'specialty'; // target _type
 const fetchDocuments = () =>
   client.fetch(
     `*[_type == $oldType && (
-      _id == "1914595c-60fd-408f-be0c-be0594ad6252" || 
-      _id == "2ddb1da1-85f6-45a5-a6d5-70f36432b832" || 
+      _id == "1914595c-60fd-408f-be0c-be0594ad6252" ||
+      _id == "2ddb1da1-85f6-45a5-a6d5-70f36432b832" ||
       _id == "99a20daa-6f79-49a8-b5ac-33560042708c"
     )][0...10] {..., "incomingReferences": *[references(^._id)]{...}}`,
     { oldType: OLD_TYPE }
@@ -18,7 +18,7 @@ const buildMutations = (docs) => {
   const mutations = [];
 
   docs.forEach((doc) => {
-    console.log("equipment", doc._id);
+    console.log('equipment', doc._id);
     // Updating a document _type field isn't allowed, we have to create a new and delete the old
     const newDocId = `${doc._id}-migrated`;
     const newDocument = { ...doc, ...{ _id: newDocId, _type: NEW_TYPE } };
@@ -29,13 +29,13 @@ const buildMutations = (docs) => {
 
     // Patch each of the incoming references
     doc.incomingReferences.forEach((referencingDocument) => {
-      console.log("ref", referencingDocument._id);
+      console.log('ref', referencingDocument._id);
       // ⚠️ We're assuming the field is named the same as the type!
       // There might be another structure involved, perhaps an array, that needs patching
       const updatedReference = {
         [NEW_TYPE]: {
           _ref: newDocId,
-          _type: "reference",
+          _type: 'reference',
         },
       };
       mutations.push({
@@ -55,6 +55,7 @@ const buildMutations = (docs) => {
 };
 
 const createTransaction = (mutations) => {
+  // eslint-disable-next-line array-callback-return
   return mutations.reduce((tx, mutation) => {
     if (mutation.patch) {
       return tx.patch(mutation.id, mutation.patch);
@@ -71,7 +72,7 @@ const createTransaction = (mutations) => {
 const migrateNextBatch = async () => {
   const documents = await fetchDocuments();
   if (documents.length === 0) {
-    console.log("No more documents to migrate!");
+    console.log('No more documents to migrate!');
     return null;
   }
   const mutations = buildMutations(documents);
