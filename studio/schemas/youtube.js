b922bf05a0a0071fbc13dcb0f758afcc57cfd012
Tomás González <tomasg88@gmail.com>
2024-01-07 18:06:22 -0300

    enhance messaging in sanity's youtube

--- studio/schemas/youtube.js
+++ studio/schemas/youtube.js
@@ -8,15 +8,19 @@ export default {
   title: 'Video de YouTube',
   fields: [
     {
+      description: 'Inserte URL del video',
       name: 'url',
       type: 'url',
       title: 'URL',
+      validation: (Rule) => Rule.required(),
     },
   ],
   components: {
     preview: ({ value }) => {
-      if (!value) return 'Seleccione un video';
+      if (!value) return 'Doble click aquí para insertar una URL';
+
       const id = getYouTubeID(value.url);
+
       return <YouTube videoId={id} />;
     },
   },
