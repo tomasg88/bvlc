aef3052bfc8337bfcd08de47035b8d0d3b90254b
Emanuel Garcia <wemagw@gmail.com>
2024-03-04 21:44:17 -0300

    Pagination component, logic in noticias and styles

--- components/Button/ButtonPagination.tsx
+++ components/Button/ButtonPagination.tsx
@@ -1,48 +1,44 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-import { cn } from "../../lib/utils"
+import { cn } from '../../lib/utils';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variants: {
-        command: "bg-red-600 text-white hover:bg-red-700 cursor-pointer",
-        pageNumber: "bg-gray-300 text-black hover:bg-gray-400 cursor-pointer"
+        command: 'bg-red-600 text-white hover:bg-red-700 cursor-pointer',
+        pageNumber: 'bg-gray-300 text-black hover:bg-gray-400 cursor-pointer',
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        default: 'h-10 px-4 py-2',
+        sm: 'h-9 rounded-md px-3',
+        lg: 'h-11 rounded-md px-8',
+        icon: 'h-10 w-10',
       },
     },
     defaultVariants: {
-      size: "default",
+      size: 'default',
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
-    return (
-      <Comp
-        className={cn(buttonVariants({ size, className }))}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Button.displayName = "Button"
+const Button = React.memo(
+  React.forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ className, size, asChild = false, ...props }, ref) => {
+      const Comp = asChild ? Slot : 'button';
+      return <Comp className={cn(buttonVariants({ size, className }))} ref={ref} {...props} />;
+    }
+  )
+);
+Button.displayName = 'Button';
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
--- pages/noticias.tsx
+++ pages/noticias.tsx
@@ -1,14 +1,38 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import Layout from 'components/Layout/Layout';
 import { sanityClient } from 'lib/sanity.client';
+import { News, NewsType } from 'types/News';
 import { allPostQuery } from 'lib/sanity.queries';
 import HeroNews from 'components/HeroNews/HeroNews';
 import CardNewsHorizontal from 'components/CardNewsHorizontal/CardNewsHorizontal';
 import styles from 'styles/PageSidebar.module.css';
-import { News, NewsType } from 'types/News';
 import { GetStaticProps } from 'next';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from 'components/Pagination/Pagination';
+
+const itemsPerPage = 5;
 
 const Noticias: FC<NewsType> = ({ list }): JSX.Element => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOflastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOflastItem - itemsPerPage;
+  const currentItems = list.slice(indexOfFirstItem, indexOflastItem);
+
+  const totalPages = Math.ceil(list.length / itemsPerPage);
+
+  const paginate = (pageNumber: number) => {
+    if (pageNumber >= 1 && pageNumber <= totalPages) {
+      setCurrentPage(pageNumber);
+    }
+  };
+
   return (
     <Layout title="Noticias">
       <div className="pb-24 bg-gray-100">
@@ -17,11 +41,28 @@ const Noticias: FC<NewsType> = ({ list }): JSX.Element => {
           <div className="max-w-5xl grid-cols-1 gap-6 px-6 pt-12 pb-24 mx-auto md:px-0">
             <div id="content" className="w-full">
               <div className="grid max-w-4xl grid-cols-1 gap-3 mx-auto ">
-                {list && list.map((n) => <CardNewsHorizontal {...n} key={n._id} />)}
+                {currentItems && currentItems.map((n) => <CardNewsHorizontal {...n} key={n._id} />)}
               </div>
             </div>
           </div>
         </div>
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious onClick={() => paginate(currentPage - 1)} />
+            </PaginationItem>
+            {Array.from({ length: totalPages }, (_, index) => index + 1).map((pageNumber) => (
+              <PaginationItem key={pageNumber} onClick={() => paginate(pageNumber)}>
+                <PaginationLink href="#" isCurrentPage={pageNumber === currentPage}>
+                  {pageNumber}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+            <PaginationItem>
+              <PaginationNext onClick={() => paginate(currentPage + 1)} />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
       </div>
     </Layout>
   );
