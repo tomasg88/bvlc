74071cd35b31b762b09bb60b0384e55e780947f3
Tomás González <tomasg88@gmail.com>
2024-03-18 23:48:47 -0300

    show visionTool plugin only on dev env

--- sanity.config.js
+++ sanity.config.js
@@ -3,15 +3,19 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { vercelDeployTool } from 'sanity-plugin-vercel-deploy';
-
 import { schemas } from './studio/schemas/schema';
+
+const plugins =
+  process.env.NODE_ENV !== 'production'
+    ? [deskTool(), visionTool(), vercelDeployTool()]
+    : [deskTool(), vercelDeployTool()];
 
 export const config = defineConfig({
   basePath: '/sanity-admin',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   name: 'bvlc',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  plugins: [deskTool(), visionTool(), vercelDeployTool()],
+  plugins,
   schema: {
     types: schemas,
   },
