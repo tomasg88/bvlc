901c4be4c5579ed5acd3ca00b5fbd481511f9d29
Tomás González <tomasg88@gmail.com>
2024-04-01 14:26:30 -0300

    new campaign schema

---
+++ studio/schemas/campaign.ts
@@ -0,0 +1,37 @@
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+  name: 'campaign',
+  title: 'Campañas',
+  type: 'document',
+  fields: [
+    defineField({
+      description: 'Nombre de la campaña que aparecerá en el sitio',
+      name: 'name',
+      title: 'Título',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'campaignLink',
+      title: 'Link de donaronline.com',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      description: 'Indicar si la campaña está activa o no',
+      initialValue: false,
+      name: 'isActive',
+      title: 'Activar',
+      type: 'boolean',
+    }),
+    defineField({
+      description:
+        'Esta opción está pensada para indicar si una campaña debe mostrarse primera. Está pensada para la campaña de Socios Protectores',
+      initialValue: false,
+      name: 'showFirst',
+      title: 'Mostrar arriba de todo',
+      type: 'boolean',
+    }),
+  ],
+});
