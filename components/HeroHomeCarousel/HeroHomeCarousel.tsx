7c660a9a48f20fc550c92954eec90637650d618f
Tomás González <tomasg88@gmail.com>
2024-05-06 11:22:47 -0300

    remove info in hero and add bottom gradient

--- components/HeroHomeCarousel/HeroHomeCarousel.tsx
+++ components/HeroHomeCarousel/HeroHomeCarousel.tsx
@@ -1,11 +1,9 @@
-import { FiPhone } from 'react-icons/fi';
 import makeCarousel from 'react-reveal/makeCarousel';
 import Fade from 'react-reveal/Fade';
-import Link from 'next/link';
 import { DEFAULT_PAGE_TITLE } from 'utils/constants';
 import CarouselUI from './CarouselUI/CarouselUI';
 import styles from './HeroHomeCarousel.module.scss';
-import { FunctionComponent, ReactElement, ReactNode } from 'react';
+import { FC, FunctionComponent, ReactElement, ReactNode } from 'react';
 import SanityImage from 'components/SanityImage/SanityImage';
 
 interface HeroHomeCarouselProps {
@@ -19,53 +17,43 @@ interface CarouselProps {
   maxTurns: number;
 }
 
-const HeroHomeCarousel: FunctionComponent<HeroHomeCarouselProps> = ({
-  arrows,
-  images = [],
-}): ReactElement => {
+const HeroHomeCarousel: FC<HeroHomeCarouselProps> = ({ arrows, images = [] }): ReactElement => {
   const Carousel = makeCarousel((props) => (
     <CarouselUI arrows={arrows} {...props} />
   )) as FunctionComponent<CarouselProps>;
 
   return (
-    <div className={styles.root}>
-      <Fade cascade delay={900}>
-        <p className={styles.legend}>Saber para servir</p>
-      </Fade>
-      <Fade delay={400}>
-        <h1 className={styles.title}>{DEFAULT_PAGE_TITLE}</h1>
-      </Fade>
+    <>
+      <div className={styles.root}>
+        <Fade cascade delay={900}>
+          <p className={styles.legend}>Saber para servir</p>
+        </Fade>
+        <Fade delay={400}>
+          <h1 className={styles.title}>{DEFAULT_PAGE_TITLE}</h1>
+        </Fade>
 
-      <div className={styles.buttonsContainer}>
-        <Link href="/contacto" className={styles.button}>
-          <FiPhone className={styles.icon} />
-          <div className={styles.text}>
-            {'Emergencias'}
-            <span className={styles.number}>{'(0261) 498-0999'}</span>
-          </div>
-        </Link>
+        <div className={styles.carouselContainer}>
+          <Carousel defaultWait={4000} maxTurns={99} /* wait for 1000 milliseconds */>
+            {images.map((img) => (
+              <Fade key={img._key}>
+                <SanityImage
+                  src={img}
+                  width={1920}
+                  height={800}
+                  fixed="true"
+                  style={{
+                    objectFit: 'cover',
+                  }}
+                  className={styles.image}
+                  priority
+                />
+              </Fade>
+            ))}
+          </Carousel>
+        </div>
       </div>
-
-      <div className={styles.carouselContainer}>
-        <Carousel defaultWait={4000} maxTurns={99} /* wait for 1000 milliseconds */>
-          {images.map((img) => (
-            <Fade key={img._key}>
-              <SanityImage
-                src={img}
-                width={1920}
-                height={800}
-                fixed="true"
-                style={{
-                  objectFit: 'cover',
-                }}
-                className={styles.image}
-                priority
-              />
-            </Fade>
-          ))}
-        </Carousel>
-      </div>
-    </div>
+      <div className="opacity-30 to-gray-100 from-gray-900 bg-gradient-to-b h-20">&nbsp;</div>
+    </>
   );
 };
 
