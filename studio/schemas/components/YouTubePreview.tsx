48e0ce1ed4e1af307b721a790ab39362d50f6565
Tomás González <tomasg88@gmail.com>
2024-06-30 13:00:26 -0300

    remove wrapper

--- studio/schemas/components/YouTubePreview.tsx
+++ studio/schemas/components/YouTubePreview.tsx
@@ -1,17 +1,11 @@
 import YouTubePlayer from 'react-youtube';
 import { PreviewProps } from 'sanity';
-import { Flex, Text } from '@sanity/ui';
+import { Text } from '@sanity/ui';
 
 export const YouTubePreview = (props: PreviewProps) => {
   const { title: url } = props;
 
-  return (
-    <Flex padding={3} align="center" justify="center">
-      {typeof url === 'string' ? (
-        <YouTubePlayer id={url} />
-      ) : (
-        <Text>Ingrese una URL de video de YouTube</Text>
-      )}
-    </Flex>
-  );
+  if (typeof url === 'string') return <YouTubePlayer id={url} />;
+
+  return <Text>Ingrese una URL de video de YouTube</Text>;
 };
