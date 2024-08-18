a20c0a49ac95eb0d4577d3bd7657b901bf15252c
Tomás González <tomasg88@gmail.com>
2024-08-18 19:51:45 -0300

    styling

--- components/HomeSections/HomeSectionCard.tsx
+++ components/HomeSections/HomeSectionCard.tsx
@@ -4,9 +4,7 @@ import Fade from 'react-reveal/Fade';
 import Image from 'next/image';
 import { DEFAULT_PAGE_IMAGE } from 'utils/constants';
 import styles from './HomeSectionCard.module.scss';
-
-const CTA_CLASSNAME =
-  'z-10 block px-4 py-2 mt-6 text-base font-bold text-center text-white bg-red-700 border-b-4 border-red-700 rounded cursor-pointer hover:bg-red-600 hover:border-red-800';
+import { FaAngleRight } from 'react-icons/fa';
 
 export type HomeSectionCardProps = {
   background: string;
@@ -27,24 +25,26 @@ export const HomeSectionCard = ({
     <div className="bg-gray-100 w-5/6 h-60 rounded-2xl flex">
       <div className={`${styles.imageClip} w-3/5 block relative`}>
         <Image
-          alt={''}
-          className={`rounded-l-2xl rounded-r-none`}
+          alt={`imágen de portada para ${title}`}
+          className={`rounded-l-2xl rounded-r-none bg-cover`}
           fill
           sizes="100vw"
           src={background || DEFAULT_PAGE_IMAGE}
         />
-        <div className="bg-black w-full h-full block relative rounded-l-2xl rounded-r-none opacity-60">
+        <div className="bg-black w-full h-full block bg-cover relative rounded-l-2xl rounded-r-none opacity-60">
+          {/* <h2 className="text-gray-100 text-2xl">{title}</h2> */}
           &nbsp;
         </div>
       </div>
       <div className="p-6 w2/5 box-border flex flex-col justify-between">
-        <div className="">
-          <p className="text-2xl">{title}</p>
-          <p className="">{description}</p>
+        <div>
+          <h2 className="text-2xl mb-2">{title}</h2>
+          <p className="font-light text-xl tracking-normal">{description}</p>
         </div>
-        <div className="w-60 block relative">
-          <Link href={href} className={CTA_CLASSNAME}>
+        <div className="relative self-end">
+          <Link href={href} className={styles.ctaButton}>
             {ctaText}
+            <FaAngleRight className="pl-2" />
           </Link>
         </div>
       </div>
